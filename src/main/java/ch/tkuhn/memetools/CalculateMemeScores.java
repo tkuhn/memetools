53baefe98c3000c9a605f79e3e899c018465e783
Tobias Kuhn <kuhntobias@gmail.com>
2013-11-25 16:31:47 +0100

    Full reimplementation of CalculateMemeScore; still needs testing

--- src/main/java/ch/tkuhn/memetools/CalculateMemeScores.java
+++ src/main/java/ch/tkuhn/memetools/CalculateMemeScores.java
@@ -1,12 +1,16 @@
 package ch.tkuhn.memetools;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import au.com.bytecode.opencsv.CSVWriter;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
@@ -15,16 +19,22 @@ import com.beust.jcommander.ParameterException;
 public class CalculateMemeScores {
 
 	@Parameter(description = "files", required = true)
-	private List<String> inputFiles = new ArrayList<>();
+	private List<String> inputFiles = new ArrayList<String>();
 
-	@Parameter(names = "-g", description = "Use <arg>-grams")
+	@Parameter(names = "-g", description = "Use X-grams")
 	private int grams = 1;
 
-	@Parameter(names = "-y", description = "Calculate scores for year <arg>")
+	@Parameter(names = "-y", description = "Calculate scores for given year")
 	private Integer year;
 
-	@Parameter(names = "-n", description = "Set n parameter to <arg>")
+	@Parameter(names = "-n", description = "Set n parameter")
 	private int n = 1;
+
+	@Parameter(names = "-fth", description = "Frequency threshold")
+	private int freqThreshold = 0;
+
+	@Parameter(names = "-nth", description = "Number threshold")
+	private int numberThreshold = 2;
 
 	public static final void main(String[] args) {
 		CalculateMemeScores obj = new CalculateMemeScores();
@@ -39,18 +49,30 @@ public class CalculateMemeScores {
 	}
 
 	private int nt;
-	private Map<String,Integer> nm = new HashMap<>();
+	private Map<String,Integer> nm;
+
+	private int et;
+	private Map<String,Integer> emm;
+	private Map<String,Integer> emx;
+	private Map<String,Integer> exm;
 
 	public CalculateMemeScores() {
 	}
 
 	public void run() {
 		for (String inputFile : inputFiles) {
-			run(inputFile);
+			run(new File(inputFile));
 		}
 	}
 
-	public void run(String inputFile) {
+	public void run(File inputFile) {
+		nt = 0;
+		nm = new HashMap<String,Integer>();
+		et = 0;
+		emm = new HashMap<String,Integer>();
+		emx = new HashMap<String,Integer>();
+		exm = new HashMap<String,Integer>();
+
 		try {
 			System.out.println("Extracting terms from input file: " + inputFile);
 			BufferedReader reader = new BufferedReader(new FileReader(inputFile));
@@ -64,9 +86,9 @@ public class CalculateMemeScores {
 					citing = citing.substring(5);
 				}
 				nt = nt + 1;
-				List<String> words = getTerms(citing);
-				Map<String,Boolean> processed = new HashMap<>();
-				for (String word : words) {
+				Map<String,Boolean> words = getTerms(citing);
+				Map<String,Boolean> processed = new HashMap<String,Boolean>();
+				for (String word : words.keySet()) {
 					if (word.length() < 2 || processed.containsKey(word)) continue;
 					if (nm.containsKey(word)) {
 						nm.put(word, nm.get(word)+1);
@@ -83,22 +105,102 @@ public class CalculateMemeScores {
 		}
 		System.out.println("Total number of documents: " + nt);
 		System.out.println("Total number of unique terms: " + nm.size());
-		// ...
+		for (String v : nm.keySet()) {
+			if (nm.get(v) < freqThreshold * nt || nm.get(v) < numberThreshold) {
+				nm.remove(v);
+			}
+		}
+
+		for (String w : nm.keySet()) {
+			emm.put(w, 0);
+			emx.put(w, 0);
+			exm.put(w, 0);
+		}
+		int errors = 0;
+		try {
+			System.out.println("Counting terms...");
+			BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				if (!line.matches(".*\\|\\|\\|.*")) {
+					errors = errors + 1;
+					continue;
+				}
+				String[] splitline = line.split("\\|\\|\\|");
+				String citing = splitline[0];
+				if (line.substring(0, 5).matches("[0-9][0-9][0-9][0-9] ")) {
+					int y = Integer.parseInt(line.substring(0, 4));
+					if (year != null && y != year) continue;
+					citing = line.substring(5);
+				}
+				Map<String,Boolean> citingTerms = getTerms(citing);
+				String cited = splitline[1];
+				Map<String,Boolean> citedTerms = getTerms(cited);
+				Map<String,Boolean> processed = new HashMap<String,Boolean>();
+				et = et + 1;
+				for (String term : citedTerms.keySet()) {
+					if (!nm.containsKey(term) || processed.containsKey(term)) continue;
+					if (citingTerms.containsKey(term)) {
+						emm.put(term, emm.get(term) + 1);
+					} else {
+						emx.put(term, emx.get(term) + 1);
+					}
+					processed.put(term, true);
+				}
+				processed = new HashMap<String,Boolean>();
+				for (String term : citingTerms.keySet()) {
+					if (!nm.containsKey(term) || processed.containsKey(term)) continue;
+					if (!citedTerms.containsKey(term)) {
+						exm.put(term, exm.get(term) + 1);
+					}
+					processed.put(term, true);
+				}
+			}
+			reader.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			System.exit(1);
+		}
+		System.out.println("Number of errors: " + errors);
+		System.out.println("Calculating meme scores and writing CSV file...");
+		try {
+			String basename = inputFile.getName().replaceAll("\\..*$", "");
+			String outputFile = "files/ms-" + basename + "-g" + grams + "-n" + n + "-y" + year + ".csv";
+			CSVWriter writer = new CSVWriter(new FileWriter(outputFile), ',', '"');
+			writer.writeNext(new String[] {"MEME SCORE", "TERM", "ABS. FREQUENCY", "REL. FREQUENCY", "MM", "M",
+					"STICKING", "XM", "X", "SPARKING", "PROPAGATION SCORE"});
+			for (String term : nm.keySet()) {
+				int em = emm.get(term) + emx.get(term);
+				int ex = et - em;
+				double stick = (double) emm.get(term) / (em + n);
+				double spark = (double) (exm.get(term) + n) / (ex + n);
+				double ps = stick / spark;
+				int absFq = nm.get(term);
+				double relFq = (double) absFq / nt;
+				double ms = ps * (absFq / nt);
+				writer.writeNext(new String[] { ms+"", term, absFq+"", relFq+"", emm.get(term)+"", em+"",
+						stick+"", exm.get(term)+"", ex+"", spark+"", ps+"" });
+			}
+			writer.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			System.exit(1);
+		}
 	}
 
-	private List<String> getTerms(String text) {
-		List<String> terms = new ArrayList<>();
+	private Map<String,Boolean> getTerms(String text) {
+		Map<String,Boolean> terms = new HashMap<String,Boolean>();
 		String[] onegrams = text.split("\\s+");
-		List<String> previous = new ArrayList<>();
+		List<String> previous = new ArrayList<String>();
 		for (String t : onegrams) {
-			terms.add(t);
+			terms.put(t, true);
 			for (int x = 1; x < grams; x++) {
 				if (previous.size() > x-1) {
 					String term = t;
 					for (int y = 0; y < x; y++) {
 						term = previous.get(y) + " " + term;
 					}
-					terms.add(term);
+					terms.put(term, true);
 				}
 			}
 			previous.add(0, t);
