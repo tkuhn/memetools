d943e70797c9f7a9588df7df903bcd9fd15a4d84
Tobias Kuhn <kuhntobias@gmail.com>
2014-01-20 11:57:29 +0100

    Some incomplete code for PMC data preparation

--- src/main/java/ch/tkuhn/memetools/PreparePmcData.java
+++ src/main/java/ch/tkuhn/memetools/PreparePmcData.java
@@ -1,18 +1,17 @@
 package ch.tkuhn.memetools;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
 
 public class PreparePmcData {
-
-	@Parameter(names = "-r", description = "Path to read raw data")
-	private String rawDataPath = "data";
-
-	@Parameter(names = "-p", description = "Path to write prepared data")
-	private String preparedDataPath = "input";
 
 	@Parameter(names = "-v", description = "Write detailed log")
 	private boolean verbose = false;
@@ -31,15 +30,69 @@ public class PreparePmcData {
 		obj.run();
 	}
 
-//	private static String pmcListFile = "PMC-ids.csv";
+	private static String pmcIdsFile = "PMC-ids.csv";
 //	private static String pmcXmlPath = "pmcoa-xml";
+
+	private static String idlinePattern = "^([^,]*,|\"[^\"]*\",){7}([^,]*|\"[^\"]*\"),(PMC[0-9]+),([0-9]*),([^,]*|\"[^\"]*\"),([^,]*|\"[^\"]*\")$";
+
+	private Map<String,String> idMap;
 
 	public PreparePmcData() {
 	}
 
 	public void run() {
+		init();
+		try {
+			makeIdMap();
+			processXmlFiles();
+			writeDataFiles();
+			writeGmlFile();
+		} catch (IOException ex) {
+			log(ex);
+			System.exit(1);
+		}
+		log("Finished");
+	}
+
+	private void init() {
+		logFile = new File(MemeUtils.getLogDir(), "prepare-pmc.log");
+		log("==========");
 		log("Starting...");
-		// TODO;
+
+		idMap = new HashMap<String,String>();
+	}
+
+	private void makeIdMap() throws IOException {
+		log("Reading IDs...");
+		File file = new File(MemeUtils.getRawDataDir(), pmcIdsFile);
+		BufferedReader r = new BufferedReader(new FileReader(file));
+		String line;
+		while ((line = r.readLine()) != null) {
+			line = line.trim();
+			if (!line.matches(idlinePattern)) {
+				log("Ignoring line: " + line);
+				continue;
+			}
+			String pmcid = line.replaceFirst(idlinePattern, "$3");
+			String doi = line.replaceFirst(idlinePattern, "$2");
+			if (!doi.isEmpty()) idMap.put(doi, pmcid);
+			String pmid = line.replaceFirst(idlinePattern, "$4");
+			if (!pmid.isEmpty()) idMap.put(pmid, pmcid);
+			log(pmcid + " " + doi + " " + pmid);
+		}
+		r.close();
+	}
+
+	private void processXmlFiles() throws IOException {
+		// TODO
+	}
+
+	private void writeDataFiles() throws IOException {
+		// TODO
+	}
+
+	private void writeGmlFile() throws IOException {
+		// TODO
 	}
 
 	private void log(Object text) {
